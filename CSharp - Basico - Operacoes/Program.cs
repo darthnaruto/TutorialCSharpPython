ebfe0f4cb4fac01e247107a504419a671302b2ec
Camilo Chaves <chaves.camilo@gmail.com>
2023-02-15 00:18:13 -0300

    Adicionado numeros binarios

--- CSharp - Basico - Operacoes/Program.cs
+++ CSharp - Basico - Operacoes/Program.cs
@@ -1,9 +1,14 @@
-﻿float num1 = 10;
-float num2 = 20;
-float soma = num1 + num2;
-float subtracao = num1 - num2;
-float multiplicacao = num1 * num2;
-float divisao = num1 / num2;
+﻿int num1 = 10;
+int num2 = 20;
+int soma = num1 + num2;
+int subtracao = num1 - num2;
+int multiplicacao = num1 * num2;
+int divisao = num1 / num2;
+
+byte numero = 1;
+numero--;
+numero--;
+Console.WriteLine(numero);
 
 Console.WriteLine(soma);
 Console.WriteLine(subtracao);
---
+++ Python - Exemplos Basicos/Operacoes.py
@@ -0,0 +1,8 @@
+num1 = 10
+num2 = 20
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(soma, subtracao, multiplicacao, divisao)
